a2268c6d54ec71fa1dde0cbc4cc233904665dbd6
Nickolas Sukhdeo <102875950+nlsukhde@users.noreply.github.com>
2024-12-24 15:12:08 -0500

    Time: 3513 ms (5.41%), Space: 18.6 MB (16.58%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,17 +1,18 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        # [2,7,11,15]
+        # 0 , 1, 2, 3
         pointer1 = 0
         pointer2 = 1
 
-        
-        while ((nums[pointer1] + nums[pointer2]) != target):
-            if pointer2 < len(nums) -1:
-                pointer2 += 1 
-            else:
+        while True:
+            if nums[pointer1] + nums[pointer2] == target:
+                return (pointer1,pointer2)
+            if pointer2 == len(nums) - 1:
                 pointer1 += 1
                 pointer2 = pointer1 + 1
+            else:
+                pointer2 += 1
 
-        
-
-        return [pointer1,pointer2]
         
