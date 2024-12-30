39c92fa1c9c31f6af1554490cb3e54b85c0abd10
Nickolas Sukhdeo <102875950+nlsukhde@users.noreply.github.com>
2024-12-29 21:17:49 -0500

    Time: 5 ms (64.27%), Space: 21.4 MB (28.57%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,28 +1,24 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        #make a frequency map
-        #sort by the maps's value
-        #add to list and return it
-
-        freq_map = {}
         
+        freqMap = {}
+
         for num in nums:
-            if num in freq_map:
-                freq_map[num] += 1 
+            if num in freqMap:
+                freqMap[num] += 1
             else:
-                freq_map[num] = 1 
+                freqMap[num] = 1
+        
+        sortedMap = sorted(freqMap.items(), key = lambda item: item[1], reverse=True)
 
-        #now we have map with frequencys
+        retval = []
 
-        sorted_keys = sorted(freq_map, key=freq_map.get, reverse=True)
+        for i in range(k):
+            retval.append(sortedMap[i][0])
+        
+        return retval
 
-        #now we have a list of the keys in the map from hightest to loweset freqs
 
-
-        #RETURN the first k elements
-        ret_list = sorted_keys[:k]
-
-        return ret_list
         
         
         
