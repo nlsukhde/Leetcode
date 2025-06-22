ab6d7d0b69b0bf914f705a6b5bb08660e19fe036
Nickolas Sukhdeo <102875950+nlsukhde@users.noreply.github.com>
2025-06-22 19:20:33 -0400

    Time: 2 ms (53.42%), Space: 18.8 MB (51.04%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,18 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
 
-        # [2,7,11,15]
-        # 0 , 1, 2, 3
-        pointer1 = 0
-        pointer2 = 1
+        hashmap = {}
 
-        while True:
-            if nums[pointer1] + nums[pointer2] == target:
-                return (pointer1,pointer2)
-            if pointer2 == len(nums) - 1:
-                pointer1 += 1
-                pointer2 = pointer1 + 1
-            else:
-                pointer2 += 1
+        for i in range(len(nums)):
+            hashmap[nums[i]] = i
 
-        
+        for i in range(len(nums)):
+            difference = target - nums[i]
+            if difference in hashmap and hashmap[difference] != i:
+                return [i, hashmap[difference]]      
