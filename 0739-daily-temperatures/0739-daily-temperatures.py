4591d751dfc3b6ce29f9f2aa8890112a69fb1b92
Nickolas Sukhdeo <102875950+nlsukhde@users.noreply.github.com>
2025-07-08 23:09:48 -0400

    Time: 148 ms (11.26%), Space: 33.5 MB (28.44%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,17 +1,12 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-    #       my_stack = []
-    # my_stack.append(10) # Push 10
-    # my_stack.append(20) # Push 20
-    # popped_element = my_stack.pop() # Pop 20
-    #  top_element = my_stack[-1] # Peek 10
 
         retval= [0] * len(temperatures)
 
         stack = []
 
         for i in range(len(temperatures)):
-            while len(stack) > 0 and temperatures[i] > stack[-1][0]:
+            while stack and temperatures[i] > stack[-1][0]:
                 retval[stack[-1][1]] = i - stack[-1][1]
                 stack.pop()
             stack.append((temperatures[i],i))
