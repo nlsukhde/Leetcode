16e1e1cb005f03ce06544cba8d06d31cb4376f95
Nickolas Sukhdeo <102875950+nlsukhde@users.noreply.github.com>
2025-07-26 15:31:23 -0400

    Time: 85 ms (84.44%), Space: 27.5 MB (66.94%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -6,10 +6,10 @@ class Solution:
         days = [0] * len(temperatures)
 
         for i in range(len(temperatures)):
-            while stack and temperatures[i] > stack[-1][0]:
-                warmer = stack.pop()
-                days[warmer[1]] = i - warmer[1]
-            stack.append((temperatures[i],i))
+            while stack and temperatures[i] > temperatures[stack[-1]]:
+                coolerIndex= stack.pop()
+                days[coolerIndex] = i - coolerIndex
+            stack.append(i)
         
         return days
 
